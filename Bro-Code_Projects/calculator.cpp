901d85529b70b61ecbe3c4816939a03fadfe856b
Jaswanth Kumar <jaswanthkumarkamireddi@gmail.com>
2026-01-08 20:19:05 +0530

    Added Bro-Code Projects

---
+++ Bro-Code_Projects/calculator.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main(){
+    char op;
+    double num1;
+    double num2;
+    double result;
+
+    std::cout<<"*******CALCULATOR*******\n";
+
+    std::cout<<"Enter either (+ - * /): ";
+    std::cin >> op;
+
+    std::cout<<"Enter #1: ";
+    std::cin>>num1;
+
+    std::cout<<"Enter #2: ";
+    std::cin>>num2;
+
+    if (op == '+'){
+        result = num1 + num2;
+        std::cout<<"Result : "<<result;
+    }else if (op == '-'){
+        result = num1 - num2;
+        std::cout<<"Result : "<<result;
+    }else if (op == '*'){
+        result = num1 * num2;
+        std::cout<<"Result : "<<result;
+    }else if (op == '/'){
+        result = num1 / num2;
+        std::cout<<"Result : "<<result;
+    }else{
+        std::cout << "Incorrect operator";
+    }
+
+    std::cout << "\n*******************************";
+    return 0;
+}
